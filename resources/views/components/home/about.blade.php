392274155c31ecd31a5b7ffdce32fdb466e21126
muamarzidan <zidanprofix93@gmail.com>
2022-08-29 15:42:14 +0700

    finish about

--- resources/views/components/home/about.blade.php
+++ resources/views/components/home/about.blade.php
@@ -4,17 +4,18 @@
             <div class="w-full lg:w-6/12 px-4">
                 <div class="flex items-center -mx-3 sm:-mx-4">
                     <div class="w-full xl:w-1/2 px-3 sm:px-4">
-                        <div class="py-3 sm:py-4">
-                            <img src="{{ url('/img/logo.png') }}" alt="" class="rounded-2xl w-full"/>
+                        <div class="py-3 sm:py-4" data-aos="fade-up" data-aos-duration="2000">
+                            <img src="{{ url('/img/about1.jpg') }}" alt="" class="rounded-2xl w-full"/>
                         </div>
-                        <div class="py-3 sm:py-4">
-                            <img src="{{ url('/img/logo-2.png') }}" alt="" class="rounded-2xl w-full"/>
+                        <div class="py-3 sm:py-4"  data-aos="fade-down" data-aos-duration="2000">
+                            <img src="{{ url('/img/about2.jpeg') }}" alt="" class="rounded-2xl w-full"/>
                         </div>
                     </div>
                     <div class="w-full xl:w-1/2 px-3 sm:px-4">
-                        <div class="my-4 relative z-10"></div>
-                        <img src="{{ url('/img/logo-3.jpg') }}" alt="" class="rounded-2xl w-full"/>
-                        <x-about-dots></x-about-dots>
+                        <div class="my-4 relative z-10" data-aos="zoom-in" data-aos-duration="2000">
+                            <img src="{{ url('/img/about3.jpeg') }}" alt="" class="rounded-2xl w-full"/>
+                            <x-about-dots></x-about-dots>
+                        </div>
                     </div>
                 </div>  
             </div>
@@ -43,5 +44,206 @@
             </div>
         </div>
     </div>
-
 </section>
+<section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20 bg-[#F3F4F6]">
+    <div class="container">
+       <div class="flex flex-wrap -mx-4">
+          <div class="w-full md:w-1/2 xl:w-1/4 px-4">
+             <div class="bg-white rounded-lg overflow-hidden mb-10">
+                <img
+                   src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
+                   alt="image"
+                   class="w-full"
+                   />
+                <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                   <h3>
+                      <a
+                         href="javascript:void(0)"
+                         class="
+                         font-semibold
+                         text-dark text-xl
+                         sm:text-[22px]
+                         md:text-xl
+                         lg:text-[22px]
+                         xl:text-xl
+                         2xl:text-[22px]
+                         mb-4
+                         block
+                         hover:text-primary
+                         "
+                         >
+                        Javascript
+                      </a>
+                   </h3>
+                   <p class="text-base text-body-color leading-relaxed mb-2">
+                        Intermediate
+                   </p>
+                   {{-- <a
+                      href="javascript:void(0)"
+                      class="
+                      inline-block
+                      py-2
+                      px-7
+                      border border-[#E5E7EB]
+                      rounded-full
+                      text-base text-body-color
+                      font-medium
+                      hover:border-primary hover:bg-primary hover:text-white
+                      transition
+                      "
+                      >
+                   View Details
+                   </a> --}}
+                </div>
+             </div>
+          </div>
+          <div class="w-full md:w-1/2 xl:w-1/4 px-4">
+             <div class="bg-white rounded-lg overflow-hidden mb-10">
+                <img
+                   src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-02.jpg"
+                   alt="image"
+                   class="w-full"
+                   />
+                <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                   <h3>
+                      <a
+                         href="javascript:void(0)"
+                         class="
+                         font-semibold
+                         text-dark text-xl
+                         sm:text-[22px]
+                         md:text-xl
+                         lg:text-[22px]
+                         xl:text-xl
+                         2xl:text-[22px]
+                         mb-4
+                         block
+                         hover:text-primary
+                         "
+                         >
+                         Javascript
+                      </a>
+                   </h3>
+                   <p class="text-base text-body-color leading-relaxed mb-2">
+                      Intermediate
+                   </p>
+                   {{-- <a
+                      href="javascript:void(0)"
+                      class="
+                      inline-block
+                      py-2
+                      px-7
+                      border border-[#E5E7EB]
+                      rounded-full
+                      text-base text-body-color
+                      font-medium
+                      hover:border-primary hover:bg-primary hover:text-white
+                      transition
+                      "
+                      >
+                   View Details
+                   </a> --}}
+                </div>
+             </div>
+          </div>
+          <div class="w-full md:w-1/2 xl:w-1/4 px-4">
+             <div class="bg-white rounded-lg overflow-hidden mb-10">
+                <img
+                   src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-03.jpg"
+                   alt="image"
+                   class="w-full"
+                   />
+                <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                   <h3>
+                      <a
+                         href="javascript:void(0)"
+                         class="
+                         font-semibold
+                         text-dark text-xl
+                         sm:text-[22px]
+                         md:text-xl
+                         lg:text-[22px]
+                         xl:text-xl
+                         2xl:text-[22px]
+                         mb-4
+                         block
+                         hover:text-primary
+                         "
+                         >
+                         Javascript
+                      </a>
+                   </h3>
+                   <p class="text-base text-body-color leading-relaxed mb-2">
+                        Intermediate
+                   </p>
+                   {{-- <a
+                      href="javascript:void(0)"
+                      class="
+                      inline-block
+                      py-2
+                      px-7
+                      border border-[#E5E7EB]
+                      rounded-full
+                      text-base text-body-color
+                      font-medium
+                      hover:border-primary hover:bg-primary hover:text-white
+                      transition
+                      "
+                      >
+                   View Details
+                   </a> --}}
+                </div>
+             </div>
+          </div>
+          <div class="w-full md:w-1/2 xl:w-1/4 px-4">
+            <div class="bg-white rounded-lg overflow-hidden mb-10">
+               <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
+                  alt="image"
+                  class="w-full"
+                  />
+               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                        >
+                       Javascript
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-2">
+                       Intermediate
+                  </p>
+                  {{-- <a
+                     href="javascript:void(0)"
+                     class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                     >
+                  View Details
+                  </a> --}}
+               </div>
+            </div>
+         </div>
+       </div>
+    </div>
+ </section>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
         <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-        
+        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     </head>
 
     <body class="antialiased text-gray-800 dark:text-gray-200">
@@ -16,5 +16,9 @@
         </div>
     <script src="{{ asset('js/app.js') }}"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
     </body>
 </html>
