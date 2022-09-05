adfd9f2748862debc20b00f92bef1e205141c425
muamarzidan <zidanprofix93@gmail.com>
2022-09-05 22:54:29 +0700

    new update ui

--- resources/views/components/home/contact.blade.php
+++ resources/views/components/home/contact.blade.php
@@ -1,3 +1,9 @@
-<div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
-</div>
+<section id:"contact" class="bg-white dark:bg-primary py-20 lg:py-[120px] overflow-hidden relative z-10">
+    <div class="container">
+        <div class="flex flex-wrap lg:justify-between -mx-4">
+            <div class="w-full lg:w-1/2 xl:w-6/12 px">
+                <div class="max"></div>
+            </div>
+        </div>
+    </div>
+</section>
